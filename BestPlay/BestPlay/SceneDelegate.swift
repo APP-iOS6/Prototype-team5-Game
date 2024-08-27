f28300c79fe6f2e6a16b6658473613ab59d9b680
kim dong kyung <kdkd422@naver.com>
2024-08-27 09:27:28 +0900

    init 프로젝트 init

---
+++ BestPlay/BestPlay/SceneDelegate.swift
@@ -0,0 +1,46 @@
+//
+//  SceneDelegate.swift
+//  BestPlay
+//
+//  Created by 김동경 on 2024/08/27.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+       
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+      
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+       
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+       
+    }
+
+
+}
+
---
+++ BestPlay/BestPlay/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BestPlay
+//
+//  Created by 김동경 on 2024/08/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
