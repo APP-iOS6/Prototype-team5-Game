9cabb353616a73fc43e9acbc65182a3b9a99bfe5
kim dong kyung <kdkd422@naver.com>
2024-08-27 17:12:19 +0900

    add 데이트 객체 익스텐션

--- BestPlay/BestPlay/Extension/Extension+date.swift
+++ BestPlay/BestPlay/Extension/Extension+date.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+extension Date {
+    //오후 3시 25분 식으로 문자열 반환하기 위한 extension
+    var formattedTime: String {
+        let formatter = DateFormatter()  //데이터 포맷터
+        formatter.locale = Locale(identifier: "ko_KR") //포맷터 지역 - 한국
+        formatter.dateFormat = "a hh:mm"    //데이터 형식
+        return formatter.string(from: self) //포맷터의 문자열 반환
+    }
+    
+    //데이터 날짜가 오늘이면 "오늘" 아니면 날짜 문자열 리턴하기위한 extension
+    var formattedDay: String {
+        let now = Date() //현재 날짜
+        let calendar = Calendar.current //캘린더를 현재로 설정
+        
+        let nowStartOfDay = calendar.startOfDay(for: now) //오늘날짜
+        let dateStartOfDay = calendar.startOfDay(for: self) //포맷터 날짜
+        
+        //날짜가 얼마나 차이나는지
+        let numOfDayDifference = calendar.dateComponents([.day], from: nowStartOfDay, to: dateStartOfDay).day
+        
+        if numOfDayDifference == 0 {
+            return "오늘" //0이면 오늘임
+        } else {
+            //그게 아니라면 포맷해서 날짜 출력
+            let formatter = DateFormatter()
+            formatter.locale = Locale(identifier: "ko_KR")
+            formatter.dateFormat = "M월 d일 E요일"
+            return formatter.string(from: self)
+        }
+    }
+}
