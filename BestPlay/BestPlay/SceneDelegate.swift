5cb7537909c5e832d2e0f35b18624160e5d9681f
Jaemin <ghdckdwn8456@gmail.com>
2024-08-27 11:28:50 +0900

    [add] Add Tabbar and items

--- BestPlay/BestPlay/SceneDelegate.swift
+++ BestPlay/BestPlay/SceneDelegate.swift
@@ -15,9 +15,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        let tabBar: UITabBarController = UITabBarController()
+        let home = ViewController()
+        let posting = ViewController()
+        let profile = ViewController()
+        
+        tabBar.setViewControllers([home, posting, profile], animated: false)
+        
+        home.tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "house.fill"), tag: 0)
+        posting.tabBarItem = UITabBarItem(title: "포스팅", image: UIImage(systemName: "plus.circle.fill"), tag: 1)
+        profile.tabBarItem = UITabBarItem(title: "프로필", image: UIImage(systemName: "person.crop.circle.fill"), tag: 2)
+        
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = ViewController()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = tabBar
         window?.makeKeyAndVisible()
     }
 
