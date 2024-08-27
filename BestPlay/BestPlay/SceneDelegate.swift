f1d66061c8afeb95c32033d6c44ae21d3cc3b26c
kim dong kyung <kdkd422@naver.com>
2024-08-27 17:17:32 +0900

    add 신델리게이트

--- BestPlay/BestPlay/SceneDelegate.swift
+++ BestPlay/BestPlay/SceneDelegate.swift
@@ -15,9 +15,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = ViewController()
+        window?.rootViewController = HomeViewController()
         window?.makeKeyAndVisible()
     }
 
