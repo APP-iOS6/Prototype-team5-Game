2396e04febc1439ee6d9e75258f2b78c58021fb0
Hoff <theloop@naver.com>
2024-08-28 15:09:46 +0900

    feat: Profile뷰에서 이미지 누르면 이미지 넣을 수 있도록 수정

--- BestPlay/BestPlay/ViewController/LoginSignUp/ProfileImageViewController.swift
+++ BestPlay/BestPlay/ViewController/LoginSignUp/ProfileImageViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ProfileImageViewController: BaseSignUpViewController {
+class ProfileImageViewController: BaseSignUpViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     private lazy var profileImageView: UIImageView = {
         let imageView = UIImageView()
@@ -19,6 +19,9 @@ class ProfileImageViewController: BaseSignUpViewController {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         imageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        imageView.isUserInteractionEnabled = true  // 이미지뷰 터치 활성화
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleImageTap))
+        imageView.addGestureRecognizer(tapGesture)
         return imageView
     }()
 
@@ -37,6 +40,41 @@ class ProfileImageViewController: BaseSignUpViewController {
         ])
         
         nextButton.addTarget(self, action: #selector(handleNextButtonTapped), for: .touchUpInside)
+    }
+
+    @objc private func handleImageTap() {
+        let alert = UIAlertController(title: "프로필 이미지", message: "이미지를 선택하세요", preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "사진첩에서 선택", style: .default, handler: { _ in
+            self.presentImagePicker()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "기본 이미지 사용", style: .default, handler: { _ in
+            self.profileImageView.image = UIImage(systemName: "person.circle")
+        }))
+        
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
+    }
+
+    private func presentImagePicker() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+
+    // UIImagePickerControllerDelegate 메서드
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[.originalImage] as? UIImage {
+            profileImageView.image = selectedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 
     override func handleNextButtonTapped() {
