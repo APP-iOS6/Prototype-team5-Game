272a7903a15288fca1e990277d00385893ab2a35
kim dong kyung <kdkd422@naver.com>
2024-08-28 17:25:16 +0900

    refactor 스택 뷰 제거 후 autolayout으로 layout다시 잡음

--- BestPlay/BestPlay/Cells/CommentTableViewCell.swift
+++ BestPlay/BestPlay/Cells/CommentTableViewCell.swift
@@ -20,10 +20,11 @@ class CommentTableViewCell: UITableViewCell {
     
     private lazy var userProfileImageView: UIImageView = {
         let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
         image.clipsToBounds = true
         image.contentMode = .scaleAspectFill
         image.image = nil
-        image.layer.cornerRadius = 70 / 2 - 10
+        image.layer.cornerRadius = 60 / 2
         return image
     }()
     
@@ -35,12 +36,32 @@ class CommentTableViewCell: UITableViewCell {
     
     private lazy var userNameLabel: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 17, weight: .bold)
         return label
     }()
     
     private lazy var commentLabel: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var dateText: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .gray
+        return label
+    }()
+    
+    private lazy var moreTextBnt: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "더보기"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .gray
         return label
     }()
     
@@ -67,39 +88,47 @@ class CommentTableViewCell: UITableViewCell {
     }
     
     func setupInterface() {
-        contentView.addSubview(mainHStack)
-        userVStack.addArrangedSubview(userNameLabel)
-        userVStack.addArrangedSubview(commentLabel)
-        
-        mainHStack.addArrangedSubview(userProfileImageView)
-        mainHStack.addArrangedSubview(userVStack)
-        
+        contentView.addSubview(userProfileImageView)
+        contentView.addSubview(userNameLabel)
+        contentView.addSubview(commentLabel)
+        contentView.addSubview(moreTextBnt)
+        contentView.addSubview(dateText)
         NSLayoutConstraint.activate([
-            mainHStack.topAnchor.constraint(equalTo: contentView.topAnchor),
-            mainHStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            mainHStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-            mainHStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            //유저 프로필 이미지 앵커
+            userProfileImageView.topAnchor.constraint(equalTo: contentView.topAnchor,constant: 10),
+            userProfileImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            userProfileImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            userProfileImageView.widthAnchor.constraint(equalToConstant: 60),
+            userProfileImageView.heightAnchor.constraint(equalToConstant: 60),
             
-            userProfileImageView.leadingAnchor.constraint(equalTo: mainHStack.leadingAnchor, constant: 12),
-            userProfileImageView.topAnchor.constraint(equalTo: mainHStack.topAnchor, constant: 12),
-            userProfileImageView.widthAnchor.constraint(equalToConstant: 70),
-            userProfileImageView.heightAnchor.constraint(equalToConstant: 70),
-            userProfileImageView.bottomAnchor.constraint(equalTo: mainHStack.bottomAnchor,constant: -12),
-            userVStack.leadingAnchor.constraint(equalTo: userProfileImageView.trailingAnchor, constant: 12),
-            userVStack.topAnchor.constraint(equalTo: userProfileImageView.topAnchor),
+            //유저 이름 앵커
+            userNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 6),
+            userNameLabel.leadingAnchor.constraint(equalTo: userProfileImageView.trailingAnchor, constant: 10),
+            
+            //댓글 라벨 앵커
+            commentLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 6),
+            commentLabel.leadingAnchor.constraint(equalTo: userNameLabel.leadingAnchor),
+            
+            //더보기 텍스트 앵커
+            moreTextBnt.leadingAnchor.constraint(equalTo: commentLabel.trailingAnchor,constant: 25),
+            moreTextBnt.bottomAnchor.constraint(equalTo: commentLabel.bottomAnchor),
+            
+            //시간 텍스트 앵커
+            dateText.leadingAnchor.constraint(equalTo: userNameLabel.trailingAnchor, constant: 10),
+            dateText.topAnchor.constraint(equalTo: userNameLabel.topAnchor),
+            dateText.bottomAnchor.constraint(equalTo: userNameLabel.bottomAnchor)
         ])
+      
       
     }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 10, right: 0))
-    }
+
     
     func updateUI(_ comment: Comment) {
         userProfileImageView.image = comment.userProfileImage
         userNameLabel.text = comment.userName
         commentLabel.text = comment.text
+        dateText.text = comment.createAt.formattedTimeAgo
     }
     
 }
