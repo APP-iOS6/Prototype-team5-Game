10cf1dd084319cae07edf1735834e1804f2fe76c
kim dong kyung <kdkd422@naver.com>
2024-08-29 14:50:24 +0900

    fix 앱 진입점 홈화면, 비로그인 시 포스팅, 마이로그인 비활성화 -> 로그인 Alert 추가

--- BestPlay/BestPlay/MainTabView/MainTabBarViewController.swift
+++ BestPlay/BestPlay/MainTabView/MainTabBarViewController.swift
@@ -11,12 +11,19 @@ import UIKit
 class MainTabBarViewController: UITabBarController, UITabBarControllerDelegate {
     
     static let shared: MainTabBarViewController = MainTabBarViewController()
+    
+    let loginVC = LoginViewController()
+    
     private var prevSelectedViewController: UIViewController? = nil
     private let loginWarningAlert = UIAlertController(title: "로그인이 필요한 기능입니다.", message: "로그인으로 이동하시겠습니까?", preferredStyle: .alert)
     private lazy var loginAction = UIAlertAction(title: "로그인", style: .default) { [weak self] _ in
         AuthenticationManager.shared.setLoggedinTrue()
         
         self?.dismiss(animated: false)
+//        let loginVC = LoginViewController()
+        let navigationController = UINavigationController(rootViewController: self?.loginVC ?? LoginViewController())
+        navigationController.modalPresentationStyle = .fullScreen
+        self?.present(navigationController, animated: true)
     }
     private let cancelAction = UIAlertAction(title: "취소", style: .cancel)
     
